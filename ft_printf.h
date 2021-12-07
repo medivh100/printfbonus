c7fde77dfed3475aa70ed8d3e3983d4e109935d8
fenix144 <yan_1731@hotmail.com>
2021-12-07 17:08:29 -0500

    Added a struct for the bonus

--- ft_printf.h
+++ ft_printf.h
@@ -1,30 +1,35 @@
-#ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+#ifndef LIBPRINTF_H
+#define LIBPRINTF_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
 
-int		ft_printf(const char *, ...);
+struct Flags
+{
+	int		flag;
+	int		minus;
+	int		minusfield;
+	int		plus;
+	int		sharp;
+	int		space;
+	int		zero;
+	int		dot;
+	int		dotstring;
+};
 
-void	strcreate(const char *, va_list, int *);
-
-int		validateflag(int);
-
-void	dispatchflag(int, va_list, int *);
-
-void	ft_putnbr_base(unsigned int, char *, int *);
-
-void	ft_putnbr(int, int *);
-
-void	ft_putstr(char *, int *);
-
-void	ft_putchar(int , int *);
-
-void	ft_putptr(long long, int *);
-
-void	ft_putptr_base(unsigned long long, char *, int *);
-
-void	ft_putuns(unsigned int, int *);
+int     		ft_printf(const char *, ...);
+void    		dispatchflag(int, va_list, int *);
+void			ft_putnbr_base(unsigned int nbr, char *base, int *res);
+void			ft_putnbr(int n, int *res);
+void    		ft_putstr(char *s, int *);
+void    		ft_putchar(int , int *);
+void			ft_putptr(long long ptr, int *res);
+void			ft_putuns(unsigned int n, int *res);
+void			ft_putptr_base(unsigned long long nbr, char *base, int *res);
+struct Flags 	initstruct(struct Flags);
+struct Flags 	flagformat(const char *s);
+void			parseformat(struct Flags format);
+int				validateflag(int c);
 
 #endif
