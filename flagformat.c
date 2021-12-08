4a6df93dab9d36e7b16fcac573bda30224ecc364
Yannick St-laurent <yan_1731@hotmail.com>
2021-12-08 15:18:09 -0500

    returning pointer to struct and removed variable length array

--- flagformat.c
+++ flagformat.c
@@ -1,12 +1,19 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-struct Flags flagformat(const char *s)
+struct Flags	*flagsformat(const char *s)
 {
-	struct Flags format;
-	format = initstruct(format);
-	while (*s)
+	int				i;
+	int				count;
+	struct Flags	*format;
+
+	format = malloc((flagnum(s)) * sizeof (struct Flags));
+	i = 0;
+	count = flagnum(s);
+	while (i < count)
 	{
-		
+		format[i] = initstruct(format[i]);
+		i++;
 	}
+	return (format);
 }
