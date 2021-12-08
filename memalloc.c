7ef94aa308b0a4d087fdb04a21268ad17045ee80
Yannick St-laurent <yan_1731@hotmail.com>
2021-12-08 13:55:08 -0500

    Function to allocate memory depending on number of flags contained within the format string

---
+++ memalloc.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include "ft_printf.h"
+
+char **memalloc(char *s)
+{
+	char **rstr;
+	int count;
+	int index;
+	int i;
+
+	i = 0;
+	index = 0;
+	count = flagnum(s);
+	rstr = malloc((count + 1) * sizeof(char *));
+	while (i < count)
+	{
+		rstr[i] = malloc((stringlen(s, &index) + 1) * sizeof(char));
+		i++;
+	}
+	rstr[i] = NULL;
+	return (rstr);
+}
