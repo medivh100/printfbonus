dabf4b68b53d7676f0a9ddce12cb3cd81b34840e
Yannick St-laurent <yan_1731@hotmail.com>
2021-12-08 13:56:17 -0500

    Finds the length of the flags and their arguments so proper amount of memory can be allocated for the format string to be split

---
+++ stringlen.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+#include "ft_printf.h"
+
+int		stringlen(char *s, int *index)
+{
+	int i;
+	int	count;
+
+	i = *index;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+			while (validateflag(s[i + 1]) == 0)
+			{
+				i++;
+				count++;
+			}
+		}
+		if (validateflag(s[i]) == 1)
+			break ;
+		i++;
+	}
+	*index = i + 1;
+	return (count + 1);
+}
