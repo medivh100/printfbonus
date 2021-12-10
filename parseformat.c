9f00599832f7b4e45a5c303f15088657bd302c78
fenix144 <yan_1731@hotmail.com>
2021-12-10 15:25:32 -0500

    still a work in progress

--- parseformat.c
+++ parseformat.c
@@ -1,7 +1,15 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	parseformat(struct Flags format, char **str)
+void	parseformat(t_flags *format, char **str)
 {
-	
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str)
+	{
+
+	}
 }
