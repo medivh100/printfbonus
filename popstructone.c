3936eb924e428aee906667b4387b1d39bed4cf18
fenix144 <yan_1731@hotmail.com>
2021-12-14 09:57:23 -0500

    Will need slight modifications

--- popstructone.c
+++ popstructone.c
@@ -9,19 +9,15 @@ t_flags popstructone(char *s, t_flags format)
 		{
 			format.minus = 1;
 			format.minusfield = minusfield(s);
-			s++;
 		}
 		if (*s == '0')
-		{
 			format.zero = 1;
-			s++;
-		}
 		if (*s == '.')
 		{
 			format.dot = 1;
 			format.dotfield = dotfield(s);
-			s++;
 		}
 	s++;
 	}
+	return (format);
 }
