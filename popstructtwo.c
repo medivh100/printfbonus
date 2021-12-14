2284922a47725762914517aefe1bb208185a450e
fenix144 <yan_1731@hotmail.com>
2021-12-14 09:58:16 -0500

    Done for now. Parse the string to extract + # space and the flag

--- popstructtwo.c
+++ popstructtwo.c
@@ -1,7 +1,28 @@
 #include "libft.h"
 #include "ft_printf.h"
 
+
+// typedef struct t_flags
+// {
+// 	int		flag;
+// 	int		plus;
+// 	int		sharp;
+// 	int		space;
+// } t_flags;
+
 t_flags	popstructtwo(char *s, t_flags format)
 {
-
+	while (*s)
+	{
+		if (*s == '+')
+			format.plus = 1;
+		if (*s == '#')
+			format.sharp = 1;
+		if (*s == ' ')
+			format.space = 1;
+		if (validateflag(*s) == 1)
+			format.flag = *s;
+		s++;
+	}
+	return (format);
 }
