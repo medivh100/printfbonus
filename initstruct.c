e165daa02d79ac3f8427779193d1238f46f6f599
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2021-12-15 10:06:30 -0500

    Added a fieldwidth int

--- initstruct.c
+++ initstruct.c
@@ -4,7 +4,7 @@
 t_flags initstruct(t_flags format)
 {
 	format.dot = 0;
-	format.dotstring = 0;
+	format.dotfield = 0;
 	format.flag = 0;
 	format.minus = 0;
 	format.minusfield = 0;
@@ -12,5 +12,6 @@ t_flags initstruct(t_flags format)
 	format.sharp = 0;
 	format.space = 0;
 	format.zero = 0;
+	format.fieldwidth = 0;
 	return (format);
 }
