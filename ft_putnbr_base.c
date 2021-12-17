9098d83ec5234ae0f3d8a1bae2fa61ef05e96e4c
fenix144 <yan_1731@hotmail.com>
2021-12-17 10:07:43 -0500

    Fixed small issue

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -10,7 +10,7 @@ void	ft_putnbr_base(t_flags format, unsigned int nbr, char *base, int *res)
 	}
 	if (nbr >= 16)
 	{
-		ft_putnbr_base(nbr / 16, base, res);
+		ft_putnbr_base(format, nbr / 16, base, res);
 		nbr = nbr % 16;
 	}
 	if (nbr <= 16 && nbr >= 0)
