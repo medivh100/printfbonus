06f797dc084e65578f6209be3834e38673c6de3b
fenix144 <yan_1731@hotmail.com>
2021-12-17 10:06:58 -0500

    Function responsible for dispatching and printing

--- printformat.c
+++ printformat.c
@@ -1,7 +1,8 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void    printformat(t_flags format, va_list ap)
+void    printformat(t_flags format, va_list ap, int *count)
 {
-    
+    if (format.flag == 'c')
+		ft_putchar(va_arg(ap, char), count);
 }
