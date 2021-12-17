f58723ff91a36c27411d3af93a75687126d5ad40
fenix144 <yan_1731@hotmail.com>
2021-12-17 10:16:55 -0500

    Added more functions for testing

--- main.c
+++ main.c
@@ -205,7 +205,7 @@ int	dotfield(char *s)
 			trigger = 1;
 			s++;
 		}
-		if ((*s > '0' && *s < '9') && trigger == 1)
+		if ((*s >= '0' && *s <= '9') && trigger == 1)
 		{
 			num = num * 10;
 			num += *s - 48;
@@ -216,16 +216,16 @@ int	dotfield(char *s)
 
 int findfieldwidth(const char *s)
 {
-    int		num;
+	int		num;
 	int		trigger;
 
 	num = 0;
 	trigger = 0;
 	while (*s)
 	{
-		if ((*s == '-' || *s == ' ' || *s == '.' || validateflag(*s) == 1) && trigger == 1)
+		if ((*s == '-' || *s == ' ' || *s == '.' || *s == '#' || validateflag(*s) == 1) && trigger == 1)
 			break ;
-		if (*s == '%')
+		if ((*s == '%' || *s == '0' || *s == '#' || *s == ' ') && (*(s + 1) <= '9' && *(s + 1) >= '0'))
 			trigger = 1;
 		if ((*s >= '0' && *s <= '9') && trigger == 1)
 		{
@@ -241,9 +241,14 @@ int main(void)
 {
 	int i;
 
-	//i = findfieldwidth("%0110.12X");
+	//i = findfieldwidth("%# -100X");
 	//printf("%d\n", i);
-	printf("%10.12x", 100);
+	printf("%#5x\n", 100);
+	printf("%-#5x\n", 100);
+	printf("%5x\n", 100);
+	printf("%-5x\n", 100);
+	printf("%#12x\n", 100);
+	printf("%-#12x\n", 100);
 	
 	return (0);
 }
