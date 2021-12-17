421cdbc60b229d66a600b6d2cfae16556b38decb
fenix144 <yan_1731@hotmail.com>
2021-12-17 09:53:41 -0500

    Now takes t_flags as an argument

--- ft_putptr.c
+++ ft_putptr.c
@@ -1,7 +1,7 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	ft_putptr(long long ptr, int *res)
+void	ft_putptr(t_flags format, long long ptr, int *res)
 {
 	*res += write(1, "0x", 2);
 	ft_putptr_base((unsigned long long) ptr, "0123456789abcdef", res);
