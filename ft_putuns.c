9fa2448d364a89679003688109dd591a6db66dbd
fenix144 <yan_1731@hotmail.com>
2022-01-05 17:26:00 -0500

    Removed t_flags variable

--- ft_putuns.c
+++ ft_putuns.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	ft_putuns(t_flags format, unsigned int n, int *res)
+void	ft_putuns(unsigned int n, int *res)
 {
 	if (n >= 10)
 	{
-		ft_putuns(format, n / 10, res);
+		ft_putuns(n / 10, res);
 		n = n % 10;
 	}
 	if (n < 10)
-		ft_putchar(format, (n + 48), res);
+		printchar((n + 48), res);
 }
