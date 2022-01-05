d6522fa6e3c66c9494a9afe7346082592093f9f6
fenix144 <yan_1731@hotmail.com>
2022-01-04 22:38:25 -0500

    No longer needed

--- ft_putptr.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-#include "ft_printf.h"
-
-void	ft_putptr(t_flags format, long long ptr, int *res)
-{
-	*res += write(1, "0x", 2);
-	ft_putptr_base(format, (unsigned long long) ptr, "0123456789abcdef", res);
-}
