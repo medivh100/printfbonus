e79f644da9cb7a7b6a30fb982a6ae2a323b7a4ba
fenix144 <yan_1731@hotmail.com>
2022-01-04 21:37:58 -0500

    Changed call of putptr to printpointer

--- printformat.c
+++ printformat.c
@@ -1,24 +1,24 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void    printformat(t_flags format, va_list ap, int *count)
+void    printformat(t_flags format, va_list ap, int *res)
 {
     if (format.flag == 'c')
-		ft_putchar(format, va_arg(ap, char), count);
+		ft_putchar(format, va_arg(ap, char), res);
 	if (format.flag == 's')
-		ft_putstr(format, va_arg(ap, char *), count);
+		ft_putstr(format, va_arg(ap, char *), res);
 	if (format.flag == 'd')
-		printnumberone(format, va_arg(ap, int), count);
+		printnumberone(format, va_arg(ap, int), res);
 	if (format.flag == 'p')
-		ft_putptr(format, va_arg(ap, long long), count);
+		printpointer(format, va_arg(ap, long long), res);
 	if (format.flag == 'i')
-		printnumberone(format, va_arg(ap, int), count);
+		printnumberone(format, va_arg(ap, int), res);
 	if (format.flag == 'u')
-		ft_putuns(format, va_arg(ap, unsigned int), count);
+		ft_putuns(format, va_arg(ap, unsigned int), res);
 	if (format.flag == 'x')
-		ft_putnbr_base(format, va_arg(ap, unsigned int), "0123456789abcdef", count);
+		ft_putnbr_base(format, va_arg(ap, unsigned int), "0123456789abcdef", res);
 	if (format.flag == 'X')
-		ft_putnbr_base(format, va_arg(ap, unsigned int), "0123456789ABCDEF", count);
+		ft_putnbr_base(format, va_arg(ap, unsigned int), "0123456789ABCDEF", res);
 	if (format.flag == '%')
-		ft_putchar(format, va_arg(ap, char), count);
+		ft_putchar(format, va_arg(ap, char), res);
 }
