392608750db9e15d632c88c953a1241f9acc5e36
fenix144 <yan_1731@hotmail.com>
2022-01-07 13:57:38 -0500

    Removed t_flags argument

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -1,7 +1,7 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	ft_putnbr_base(t_flags format, unsigned int nbr, char *base, int *res)
+void	ft_putnbr_base(unsigned int nbr, char *base, int *res)
 {	
 	if (nbr < 0)
 	{
@@ -10,7 +10,7 @@ void	ft_putnbr_base(t_flags format, unsigned int nbr, char *base, int *res)
 	}
 	if (nbr >= 16)
 	{
-		ft_putnbr_base(format, nbr / 16, base, res);
+		ft_putnbr_base(nbr / 16, base, res);
 		nbr = nbr % 16;
 	}
 	if (nbr <= 16 && nbr >= 0)
