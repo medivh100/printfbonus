c80766f0a73141f51782e70829012cb38c3b0fc0
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:51:36 -0500

    Function for printing flag x/X with various options

--- printhex.c
+++ printhex.c
@@ -3,5 +3,15 @@
 
 void	printhex(t_flags format, unsigned int nbr, char *base, int *res)
 {
-	
+	if (format.sharp == 1 && format.minus == 0)
+		printhexsharp(format, nbr, base, res);
+	if (format.sharp == 0 && format.minus == 0)
+		printhexreg(format, nbr, res);
+	if (format.minus == 1)
+		printhexminusopt(format, nbr, base, res);
+	if (format.sharp == 1 && format.dot == 0 && format.fieldwidth == 0 && format.minus == 0)
+		printox(base, res);
+	ft_putnbr_base(nbr, base, res);
+	if (format.minus == 1)
+		printhexminus(format, nbr, res);
 }
