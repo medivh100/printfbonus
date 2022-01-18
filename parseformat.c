571b4018209f66121e53c17340364737012945c9
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:46:13 -0500

    Removed the initial call to initstruct as it is no longer necessary

--- parseformat.c
+++ parseformat.c
@@ -1,10 +1,9 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	parseformat(t_flags format, char *str, va_list ap, int *count)
+void	parseformat(t_flags format, char *str, va_list ap, int *res)
 {
 	format = popstructone(str, format);
 	format = popstructtwo(str, format);
-	free(str);
-	printformat(format, ap, count);
+	printformat(format, ap, res);
 }
