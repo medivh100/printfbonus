7bba87035960db3b679f7b898b9ae37e87240827
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:49:53 -0500

    Function called by ft_printf that dispatches format to be populated and print given argument depending on what's parsed. Also prints the rest of the format string

---
+++ printall.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+#include "ft_printf.h"
+
+void	printall(va_list ap, const char *s, char **formatstrings, int *res)
+{
+	t_flags	format;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s[j])
+	{
+		while (s[j] == '%')
+		{
+			format = initstruct(format);
+			parseformat(format, formatstrings[i], ap, res);
+			j += ft_strlen(formatstrings[i]);
+			free(formatstrings[i]);
+			i++;
+			if (s[j] == '\0')
+				break;
+		}
+		if (s[j] == '\0')
+			break ;
+		if (s[j] != '%')
+			printchar(s[j], res);
+		j++;
+	}
+}
