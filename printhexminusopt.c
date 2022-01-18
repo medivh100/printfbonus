08bc262718e84bc3ddffb3a52b32622443972cfa
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:54:16 -0500

    Similar to printhexminus

---
+++ printhexminusopt.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+#include "ft_printf.h"
+
+void	printhexminusopt(t_flags format, unsigned int nbr, char *base, int *res)
+{
+	if (format.dot == 1 && format.sharp == 0)
+		while ((--format.dotfield - memsizebase(nbr)) >= 0)
+			*res += write(1, "0", 1);
+	if (format.dot == 1 && format.sharp == 1)
+	{
+		printox(base, res);
+		while ((--format.dotfield - memsizebase(nbr)) >= 0)
+			*res += write(1, "0", 1);
+	}
+	if (format.dot == 0 && format.sharp == 1)
+		printox(base, res);
+}
