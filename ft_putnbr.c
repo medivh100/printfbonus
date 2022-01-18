8fc06b2738e00af0615afd88eb5df304062c5b1b
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:34:51 -0500

    Can't remember what I changed(if anything)

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,17 +1,16 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	ft_putnbr(t_flags format, int n, int *res)
+void	ft_putnbr(int n, int *res)
 {
-	long long nout = (long long) n;
+	long long	nout;
+
+	nout = (long long) n;
 	if (nout < 0)
-	{
-		printchar('-', res);
 		nout = -nout;
-	}
 	if (nout >= 10)
 	{
-		ft_putnbr(format, nout / 10, res);
+		ft_putnbr(nout / 10, res);
 		nout = nout % 10;
 	}
 	if (nout <= 9 && nout >= 0)
