f5b14f77ab56ac0344b51a70f64e4d372be3d4fd
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:26:29 -0500

    Made changes so it works with various types of format strings

--- findfieldwidth.c
+++ findfieldwidth.c
@@ -3,8 +3,8 @@
 
 int findfieldwidth(const char *s)
 {
-    int		num;
-	int		trigger;
+    int	num;
+	int	trigger;
 
 	num = 0;
 	trigger = 0;
@@ -12,7 +12,8 @@ int findfieldwidth(const char *s)
 	{
 		if ((*s == ' ' || *s == '.' || validateflag(*s) == 1) && trigger == 1)
 			break ;
-		if ((*s == '%' || *s == '0' || *s == '#' || *s == ' ' || *s == '-') && (*(s + 1) <= '9' && *(s + 1) >= '0'))
+		if ((*s == '%' || *s == '0' || *s == '#' || *s == ' '
+			|| *s == '-' || *s == '+') && (*(s + 1) <= '9' && *(s + 1) >= '1'))
 			trigger = 1;
 		if ((*s >= '0' && *s <= '9') && trigger == 1)
 		{
