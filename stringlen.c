14d02976038c37c7ef1a27dadc844996dc25d96c
fenix144 <yan_1731@hotmail.com>
2022-01-18 14:59:19 -0500

    Changed all instances of res to count

--- stringlen.c
+++ stringlen.c
@@ -4,10 +4,10 @@
 int		stringlen(const char *s, int *index)
 {
 	int i;
-	int	res;
+	int	count;
 
 	i = *index;
-	res = 0;
+	count = 0;
 	while (s[i])
 	{
 		if (s[i] == '%')
@@ -15,7 +15,7 @@ int		stringlen(const char *s, int *index)
 			while (validateflag(s[i + 1]) == 0)
 			{
 				i++;
-				res++;
+				count++;
 			}
 		}
 		if (validateflag(s[i]) == 1)
@@ -23,5 +23,5 @@ int		stringlen(const char *s, int *index)
 		i++;
 	}
 	*index = i + 1;
-	return (res + 1);
+	return (count + 1);
 }
--- subflag.c
+++ subflag.c
@@ -3,29 +3,14 @@
 
 char	**subflag(const char *s, char **str)
 {
-	int		index;
-	int		i;
-	int		j;
+	int	iter;
+	int	limit;
+	int index;
 
-	str = memalloc(s);
-	i = 0;
-	j = 0;
+	iter = -1;
+	limit = flagnum(s);
 	index = 0;
-	while (s[index])
-	{
-		if (s[index] == '%')
-		{
-			while (s[index + i] != ' ')
-			{
-				str[j][i] = s[index + i];
-				i++;
-				if (s[index + i] == '%')
-					break ;
-			}
-		i = 0;
-		j++;
-		}
-	index++;
-	}
+	while (++iter < limit)
+		str[iter] = findformat(s,str[iter], &index);
 	return (str);
 }
