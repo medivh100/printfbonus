6511c02cb4d3a47128d04c2c0ef3a4878675c9b7
fenix144 <yan_1731@hotmail.com>
2022-01-18 16:51:27 -0500

    Renamed all the files to _bonus

--- parseformat.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-#include "ft_printf.h"
-
-void	parseformat(t_flags format, char *str, va_list ap, int *res)
-{
-	format = popstructone(str, format);
-	format = popstructtwo(str, format);
-	printformat(format, ap, res);
-}
--- printchar.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-#include "ft_printf.h"
-
-void	printchar(int c, int *res)
-{
-	*res += write(1, &c, 1);
-}
