098e9968d45b00eab980a6689c2f1adf5f811314
fenix144 <yan_1731@hotmail.com>
2022-01-19 12:41:19 -0500

    Modified some function definitions

--- ft_printf_bonus.h
+++ ft_printf_bonus.h
@@ -18,13 +18,12 @@ typedef struct t_flags
 	int		dotfield;
 } t_flags;
 
-int     		ft_printf(const char *, ...);
-void			ft_putnbr_base(unsigned int nbr, char *base, int *res);
-void			ft_putnbr_bonus(int n, int *res);
 void    		ft_putstr_bonus(t_flags format, char *s, int *);
 void    		ft_putchar(t_flags format, int , int *);
 t_flags		 	initstruct(t_flags);
+int				findfieldwidth(const char *s);
 void			parseformat(t_flags format, char *str, va_list ap, int *count);
+char			*findformat(const char *s,char *str, int *index);
 int				flagnum(const char *s);
 int				stringlen(const char *s, int *index);
 char 			**memalloc(const char *s);
@@ -34,7 +33,6 @@ t_flags			popstructtwo(const char *s, t_flags format);
 int				minusfield(char *s);
 int				dotfield(const char *s);
 void    		printformat(t_flags format, va_list ap, int *res);
-void			printchar(int c, int *res);
 void			printnumberone(t_flags format, int n, int *res);
 void			printnumbertwo(t_flags format, int n, int *res);
 void			printnumberthree(t_flags format, int n, int *res);
@@ -42,15 +40,18 @@ void			printnumminustwo(t_flags format, int n, int *res);
 void			printnumminus(t_flags format, int n, int *res);
 void			printnumzero(t_flags format, int n, int *res);
 int 			numsizedot(long long n);
+int				numsize(long long n);
 void			printpointer(t_flags format, long long ptr, int *res);
 char			*ft_itoa_base(long long n);
 void			printunsigned(t_flags format, unsigned int n, int *res);
 void			printhex(t_flags format, unsigned int nbr, char *base, int *res);
+void			printhexsharp(t_flags format, unsigned int nbr, char *base, int *res);
 void			printhexminus(t_flags format, unsigned int nbr, int *res);
 void			printhexminusopt(t_flags format, unsigned int nbr, char *base, int *res);
 void			printhexreg(t_flags format, unsigned int nbr, int *res);
 void			printox(char *base, int *res);
 void			printpercent(t_flags format, int *res);
 int				memsizebase(long long n);
+void			printall(va_list ap, const char *s, char **formatstrings, int *res);
 
 #endif
