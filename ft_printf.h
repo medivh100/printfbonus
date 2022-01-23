30c1e9c807bcd503e02475a865b59bc718e3aac6
fenix144 <yan_1731@hotmail.com>
2022-01-23 11:25:23 -0500

    Added for mandatory part

--- ft_printf.h
+++ ft_printf.h
@@ -5,10 +5,10 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-int		ft_printf(const char *, ...);
-void	strcreate(const char *, va_list, int *);
+int		ft_printf(const char *s, ...);
+void	strcreate(const char *s, va_list *ap, int *res);
 int		validateflag(int c);
-void	dispatchflag(int, va_list, int *);
+void	dispatchflag(int, va_list *ap, int *res);
 void	ft_putnbr_base(unsigned int, char *, int *);
 void	ft_putnbr(int, int *);
 void	ft_putstr(char *, int *);
