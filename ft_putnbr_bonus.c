68abe435acd13fbf309b02bd0371a6f7f60f23bd
fenix144 <yan_1731@hotmail.com>
2022-01-23 11:27:18 -0500

    Added a clause for nbr == 0

--- ft_putnbr_bonus.c
+++ ft_putnbr_bonus.c
@@ -2,16 +2,18 @@
 #include "ft_printf_bonus.h"
 #include "ft_printf.h"
 
-void	ft_putnbr(int n, int *res)
+void	ft_putnbr_bonus(t_flags format, int n, int *res)
 {
 	long long	nout;
 
 	nout = (long long) n;
+	if (format.dot == 1 && format.dotfield == 0 && n == 0)
+		return ;
 	if (nout < 0)
 		nout = -nout;
 	if (nout >= 10)
 	{
-		ft_putnbr(nout / 10, res);
+		ft_putnbr_bonus(format, nout / 10, res);
 		nout = nout % 10;
 	}
 	if (nout <= 9 && nout >= 0)
