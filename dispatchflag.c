113f730e4f0f9965a7f2aa4b1e6402b7e8ac2e70
fenix144 <yan_1731@hotmail.com>
2022-01-23 11:22:01 -0500

    Added for the mandatory part

--- dispatchflag.c
+++ dispatchflag.c
@@ -1,24 +1,24 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void	dispatchflag(int c, va_list ap, int *res)
+void	dispatchflag(int c, va_list *ap, int *res)
 {
 	if (c == 'c')
-		printchar(va_arg(ap, int), res);
+		printchar(va_arg(*ap, int), res);
 	if (c == 's')
-		ft_putstr(va_arg(ap, char *), res);
+		ft_putstr(va_arg(*ap, char *), res);
 	if (c == 'd')
-		ft_putnbr(va_arg(ap, int), res);
+		ft_putnbr(va_arg(*ap, int), res);
 	if (c == 'p')
-		ft_putptr(va_arg(ap, long long), res);
+		ft_putptr(va_arg(*ap, long long), res);
 	if (c == 'i')
-		ft_putnbr(va_arg(ap, int), res);
+		ft_putnbr(va_arg(*ap, int), res);
 	if (c == 'u')
-		ft_putuns(va_arg(ap, unsigned int), res);
+		ft_putuns(va_arg(*ap, unsigned int), res);
 	if (c == 'x')
-		ft_putnbr_base(va_arg(ap, unsigned int), "0123456789abcdef", res);
+		ft_putnbr_base(va_arg(*ap, unsigned int), "0123456789abcdef", res);
 	if (c == 'X')
-		ft_putnbr_base(va_arg(ap, unsigned int), "0123456789ABCDEF", res);
+		ft_putnbr_base(va_arg(*ap, unsigned int), "0123456789ABCDEF", res);
 	if (c == '%')
 		*res += write(1, "%", 1);
 }
