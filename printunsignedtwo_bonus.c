ed11aaea6b56cb1b109789081c17607f194787e5
fenix144 <yan_1731@hotmail.com>
2022-02-23 09:52:58 -0500

    Final normed

--- printunsignedtwo_bonus.c
+++ printunsignedtwo_bonus.c
@@ -1,13 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printunsignedtwo_bonus.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yst-laur <yst-laur@student.42quebec.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 19:39:30 by yst-laur          #+#    #+#             */
+/*   Updated: 2022/01/28 19:39:32 by yst-laur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 #include "ft_printf.h"
 #include "ft_printf_bonus.h"
 
 void	printunsignedtwo(t_flags format, unsigned int n, int *res)
 {
-	if (format.fieldwidth > 0 && format.minus == 0 && format.dot == 0 && format.zero == 0)
+	if (format.fieldwidth > 0 && format.minus == 0
+		&& format.dot == 0 && format.zero == 0)
 		while ((--format.fieldwidth - numsize(n)) >= 0)
 			*res += write(1, " ", 1);
-	if (format.fieldwidth > 0 && format.minus == 0 && format.dot == 0 && format.zero == 1)
+	if (format.fieldwidth > 0 && format.minus == 0
+		&& format.dot == 0 && format.zero == 1)
 		while ((--format.fieldwidth - numsize(n)) >= 0)
 			*res += write(1, "0", 1);
 	if (format.fieldwidth > 0 && format.dot == 1 && format.minus == 0)
