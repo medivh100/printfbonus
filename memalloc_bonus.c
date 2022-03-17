69ffa0203579894e86da688c7b4d85033dd0436c
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2022-03-17 12:36:03 -0400

    Using memset to clear all garbage from the string

--- memalloc_bonus.c
+++ memalloc_bonus.c
@@ -20,12 +20,19 @@ char	**memalloc(const char *s)
 	int		index;
 	int		i;
 
+	int mem;
+
 	i = -1;
 	index = 0;
 	res = flagnum(s);
 	rstr = malloc((res + 1) * sizeof(char *));
 	while (++i < res)
-		rstr[i] = malloc((stringlen(s, &index) + 1) * sizeof(char));
+	{
+		mem = stringlen(s, &index);
+		rstr[i] = malloc((mem + 2) * sizeof(char));
+		ft_memset(rstr[i], '\0', (mem + 2));
+	}
+	//rstr[i] = malloc(stringlen(s, &index) * sizeof(char));
 	rstr[i] = NULL;
 	return (rstr);
 }
